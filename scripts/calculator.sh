5f5656b4805445fca3ecf01cab95be6a309eb526
ad44920 <arnesh93d@gmail.com>
2024-11-09 23:20:45 +0530

    Updated Calculator Script

--- scripts/calculator.sh
+++ scripts/calculator.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#############################################
+#AUTHOR : ARNESH DEY
+#COMPANY : KRONOS INDIA
+#PROGRAM : CALCULATOR APP
+#############################################
+
 echo "Enter the first Number"
 read a
 echo "Enter Second Number"
@@ -12,19 +19,19 @@ echo "4. Division"
 read ch
 
 case $ch in 
-	1)ans=`$a+$b`
+	1)ans=$((a+b))
 	echo "sum of the numbers $a + $b is"
 	;;
 
-	2)ans=`$a-$b`
+	2)ans=$((a-b))
 	echo "Subtraction of the numbers $a - $b is"
 		;;
 	
-	3)ans=`$a*$b`
+	3)ans=$((a*b))
 	echo "Multiplication of the numbers $a X $b is"
                 ;;
 	
-	4)ans=`$a/$b`
+	4)ans=$((a/b))
 	echo "Division of the numbers $a / $b is"
                 ;;
 esac
