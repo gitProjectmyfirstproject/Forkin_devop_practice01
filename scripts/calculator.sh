2f19586c04a0b8c450105f0f2a8387ca093fca03
ad44920 <arnesh93d@gmail.com>
2024-11-09 23:16:22 +0530

    First Commit into calculator application

---
+++ scripts/calculator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Enter the first Number"
+read a
+echo "Enter Second Number"
+read b
+# Input type of operation
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+
+case $ch in 
+	1)ans=`$a+$b`
+	echo "sum of the numbers $a + $b is"
+	;;
+
+	2)ans=`$a-$b`
+	echo "Subtraction of the numbers $a - $b is"
+		;;
+	
+	3)ans=`$a*$b`
+	echo "Multiplication of the numbers $a X $b is"
+                ;;
+	
+	4)ans=`$a/$b`
+	echo "Division of the numbers $a / $b is"
+                ;;
+esac
+echo "ANSWER : $ans" 
+
+	
